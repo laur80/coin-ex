f36f688277cb99dc2c666b41ff8b7a49e9d47d69
laur80 <laur80raul@gmail.com>
2020-10-22 16:48:05 +0300

    Updates

---
+++ precache-manifest.292b58d0fe3de0585c77cb8321629f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6eed9354918bc30a538ad906366a13b",
+    "url": "/coin-ex/index.html"
+  },
+  {
+    "revision": "e1e8c3aafad54490611c",
+    "url": "/coin-ex/static/css/main.910ac69f.chunk.css"
+  },
+  {
+    "revision": "2c4231dcf663ec4e9804",
+    "url": "/coin-ex/static/js/2.1cf4ce6e.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/coin-ex/static/js/2.1cf4ce6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e8c3aafad54490611c",
+    "url": "/coin-ex/static/js/main.00cac574.chunk.js"
+  },
+  {
+    "revision": "ebe10e4e6866f92e8c93",
+    "url": "/coin-ex/static/js/runtime-main.0512a76c.js"
+  }
+]);
